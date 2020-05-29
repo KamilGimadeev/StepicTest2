ad5ddb5f064c851041b57a22360003da28e6be21
KamilGimadeev <kamilisodon@gmail.com>
2020-05-29 15:48:26 +0400

    Updated2

--- test_items.py
+++ test_items.py
@@ -3,10 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
 try:
-    bcs = "button.btn-add-to-basket"
-    def test_guest_should_see_login_link(browser):
+    bcs = "button.bt-add-to-basket"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    def test_added_button(browser):
+         browser.get(link)
          # browser.implicitly_wait(5)
          assert browser.find_element(By.CSS_SELECTOR, bcs) , "Кнопка не найдена"
          button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, bcs))
