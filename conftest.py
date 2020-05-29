59005af637430754981037c4acd7724b5a0892c0
KamilGimadeev <kamilisodon@gmail.com>
2020-05-29 12:58:38 +0400

    Updated file

--- conftest.py
+++ conftest.py
@@ -1,23 +1,21 @@
 import pytest
 from selenium import webdriver
-
+site = 0
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='none',
+                     help="Choose interface language")
 
 
-@pytest.fixture(scope="function")
+
+@pytest.fixture(params = ["Chrome","Firefox"])
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
+    language = request.config.getoption("language")
+    brow = request.param
+    if brow == "Chrome" :
         browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
+    elif brow == "Firefox":
         browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    browser.get(link)
     yield browser
-    print("\nquit browser..")
     browser.quit()
